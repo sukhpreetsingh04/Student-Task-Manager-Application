086d58db333032bccb2862aa8ba1857838cf39c8
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-05-26 11:12:15 +0530

    Updated CompleteTasks, DashBoard, and ProductivitySummary of Student Task Manager Application.

--- app/src/main/java/com/application/studenttaskmanager/components/TopApplicationBar.kt
+++ app/src/main/java/com/application/studenttaskmanager/components/TopApplicationBar.kt
@@ -14,14 +14,33 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopApplicationBar(
     content: @Composable (PaddingValues) -> Unit
 ) {
+
+    val menuStatus = remember() {
+        mutableStateOf(false)
+    }
+
+    val menuItems = listOf(
+        "All Tasks",
+        "Personal",
+        "Wishlist",
+        "Your Favourites",
+        "DeadLines",
+        "Your Progress",
+        "Analytics and Weekly Reports",
+        "Settings",
+        "Logout"
+    )
 
 
     Scaffold(
@@ -37,14 +56,19 @@ fun TopApplicationBar(
                     }
                 },
                 actions = {
-                    IconButton(onClick = {  }) {
-                        Icon(Icons.Filled.Share, contentDescription = "Share icon")
-                    }
-                    IconButton(onClick = {  }) {
+                    IconButton(onClick = {}) {
                         Icon(Icons.Filled.Search, contentDescription = "Search icon")
                     }
-                    IconButton(onClick = {  }) {
+                    IconButton(onClick = {menuStatus.value = true}) {
                         Icon(Icons.Filled.MoreVert, contentDescription = "More icon")
+
+                        DropdownMenu(
+                            expanded = menuStatus.value,
+                            onDismissRequest = { menuStatus.value = false }) {
+                            menuItems.forEach { DropdownMenuItem(text = { Text(text = it) }, onClick = {
+                                menuStatus.value = false
+                            }) }
+                        }
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -54,8 +78,7 @@ fun TopApplicationBar(
                 )
             )
         },
-        content = {
-            paddingValues ->
+        content = { paddingValues ->
             content(paddingValues)
         }
     )
