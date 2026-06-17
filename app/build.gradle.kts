6134107ab1b4018b1c8092147885b16c541e5639
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-06-17 14:59:22 +0530

    Changes in build.gradle and gradle.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.application.studenttaskmanager"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.application.studenttaskmanager"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
