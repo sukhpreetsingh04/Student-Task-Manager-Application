deb934da0700e27e94dd107f93c7ba9f3d425fc5
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-07-30 11:23:05 +0530

    fix(task-screen): prevent dialog crash by replacing rememberSaveable

--- app/src/main/java/com/application/studenttaskmanager/screens/TaskScreen.kt
+++ app/src/main/java/com/application/studenttaskmanager/screens/TaskScreen.kt
@@ -24,13 +24,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.application.studenttaskmanager.components.TaskScreenItems
+import com.application.studenttaskmanager.components.task.TaskScreenItems
 import com.application.studenttaskmanager.data.TaskDraft
 import com.application.studenttaskmanager.data.TaskItem
 
@@ -45,7 +45,7 @@ fun TaskScreen(
 ) {
     val context = LocalContext.current
 
-    var draft by rememberSaveable {
+    var draft by remember {
         mutableStateOf(
             TaskDraft(
                 title = task?.title ?: "",
